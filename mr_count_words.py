8f65304237d819aea41aa29e326874685ebe1d41
bhigley <bhigley@zagmail.gonzaga.edu>
2022-01-27 16:28:54 -0800

    some logging completed

--- mr_count_words.py
+++ mr_count_words.py
@@ -1,6 +1,7 @@
+from fileinput import filename
 import logging
 import multiprocessing
-#import string
+import string
 import operator
 import glob
 import time
@@ -11,6 +12,8 @@ from basicMR import SimpleMapReduce
 logging.basicConfig(filename='count_words.log', level= logging.INFO)
 
 def find_words(file_name):
+    logging.info("Starting find_words function")
+    start_time = time.perf_counter()
     STOP_WORDS = set([
             'a', 'an', 'and', 'are', 'as', 'be', 'by', 'for', 'if', 'in', 
             'is', 'it', 'of', 'or', 'py', 'rst', 'that', 'the', 'to', 'with',
@@ -27,46 +30,39 @@ def find_words(file_name):
                 word = word.lower()
                 if word.isalpha() and word not in STOP_WORDS:
                     output.append( (word, 1) )
+                else:
+                    logging.info('(' + word + ')' + " is a stop word or not alphabetical")
+    end_time = time.perf_counter()
+    logging.info("Time to create uncounted list: " + str(end_time - start_time))
+    logging.info("List of uncounted words: " + str(output))
     return output
 
 def count_words(item):
     logging.info('Starting count words function')
-    start_time = time.perf_counter()
 
+    start_time = time.perf_counter()
     word, occurances = item
+    end_time = time.perf_counter()
+
+    logging.info(word + ' count completed, time: ' + str(end_time - start_time))
+
     return (word, sum(occurances))
 
 def main():
-    
     logging.basicConfig(filename='count_words.log', level=logging.DEBUG)
     logging.info('Starting count words main program')
+    start_time = time.perf_counter()
 
-   # input_files = glob.glob('/Users/benhigley/Desktop/CPSC_313/test.txt')
     input_files = glob.glob('*.txt', recursive=True)
-    
     mapper = SimpleMapReduce(find_words, count_words)
-    word_counts = mapper(input_files)
+    word_counts = mapper(input_files) # calls __call__
     print(word_counts)
-    word_counts.sort(key=operator.itemgetter(1))
-    word_counts.reverse()
+    logging.info("Final word counts: " + str(word_counts))
+    #word_counts.sort(key=operator.itemgetter(1))
+    #word_counts.reverse()
 
-# delete later
-def create_file(source_words, filename, max_file_size):
-    with open(filename, 'w') as current_file:
-        word_counts = {}
-        cur_file_size = 0
-        while cur_file_size < max_file_size:
-            source_words_size = len(source_words)
-            random_index = randint(0, source_words_size - 1)
-            random_word = source_words[random_index]
-            current_file.write(random_word + ' ')
-            cur_file_size += len(random_word) + 1
-            word_counts[random_word] = word_counts.get(random_word, 0) + 1
-
-def create_source_words(count_words):
-    # Call a simple dictionary service (found by searching) to get the random word list
-    resp = requests.get(f"https://random-word-api.herokuapp.com/word?number={count_words}")
-    return list(resp.json())
+    end_time = time.perf_counter()
+    logging.info("Total map reduce time: " + str(end_time - start_time))
 
 if __name__ == '__main__':
     main()
